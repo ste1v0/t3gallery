5b14b1de44f319de6a1cec4af5f38b89ca862410
ste1v0 <databusybee@gmail.com>
2024-05-11 15:13:44 +0300

    page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = 'force-dynamic'
 
-export default async function HomePage() {
+async function Images() {
 
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id)
   })
-
+  
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map(image => (
           <div key={image.id} className="w-48 flex flex-col">
             <img src={image.url} />
@@ -18,7 +18,19 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
-      Hello (gallery in progress)
+  )
+}
+
+export default async function HomePage() {
+
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
